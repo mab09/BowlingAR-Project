02cfb520b875fa77a381d8f0eacf7017fbe21647
KageRyu <m.alibeg@gmail.com>
2024-11-29 15:17:13 +0530

    Spawn Bowling lane

--- Assets/Scripts/PinDeckController.cs
+++ Assets/Scripts/PinDeckController.cs
@@ -13,6 +13,20 @@ public class PinDeckController : MonoBehaviour
     private Transform _pinDeckSpawnPoint;
     private bool _pinDeckCreated = false;
 
+    private void Awake()
+    {
+        // set ar camera position for editor testing
+#if UNITY_EDITOR
+        _arCamera.transform.position = new Vector3(0, 1.4f, 4);
+        _arCamera.transform.eulerAngles = new Vector3(
+            _arCamera.transform.eulerAngles.x,
+            _arCamera.transform.eulerAngles.y + 180,
+            _arCamera.transform.eulerAngles.z
+        );
+#endif
+    }
+
+    // Called from content positioning behavior (a Vuforia component inside Plane Finder)
     public void CreatePinDeck()
     {
         StartCoroutine(SetupBowlingLaneRoutine());
@@ -20,7 +34,33 @@ public class PinDeckController : MonoBehaviour
 
     private IEnumerator SetupBowlingLaneRoutine()
     {
-        yield return null;
+        // Get plane indicator's transform
+        Transform defaultPlaneIndicator = _planeFinder.PlaneIndicator.transform;
+
+        // Gets camera direction for rotation
+        Vector3 directionTowardsCamera = defaultPlaneIndicator.position + _arCamera.position;
+
+        // invert direction for editor testing 
+#if UNITY_EDITOR
+        directionTowardsCamera = defaultPlaneIndicator.position - _arCamera.position;
+#endif
+
+        // Reset the Y position to keep the lane straight
+        directionTowardsCamera.y = 0;
+
+        // Store rotation of the track to face ARCamera
+        Quaternion lookRotation = Quaternion.LookRotation(-directionTowardsCamera, Vector3.up);
+
+        // Create a new pin deck base
+        GameObject bowlingLaneClone = Instantiate(_bowlingLanePrefab, defaultPlaneIndicator.position, lookRotation);
+
+        // Get position and rotation for new pin deck from the spawn point
+        _pinDeckSpawnPoint = bowlingLaneClone.transform.Find("PinDeckSpawnPoint");
+
+        // Creates a new pin deck
+        _pinDeckClone = Instantiate(_pinDeckPrefab, _pinDeckSpawnPoint.position, _pinDeckSpawnPoint.rotation);
+
+        yield return new WaitForSeconds(1);
     }
 
     // Update is called once per frame
